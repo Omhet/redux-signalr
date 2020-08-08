a0ac14bff92aea2b0ba90f1a617b3d04cb6e1069
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-08-08 16:14:34 +0300

    Prettify types

--- src/types.ts
+++ src/types.ts
@@ -1,50 +1,54 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as signalR from "@microsoft/signalr";
-import { withCallbacks } from './helpers';
+import { withCallbacks } from "./helpers";
 
 export interface AnyAction extends Action {
-    // Allows any extra properties to be defined in an action.
-    [extraProps: string]: any
+  // Allows any extra properties to be defined in an action.
+  [extraProps: string]: any;
 }
 
 export interface Action<T = any> {
-    type: T
+  type: T;
 }
 
 export interface Dispatch<A extends Action = AnyAction> {
-    <T extends A>(action: T): T
+  <T extends A>(action: T): T;
 }
 
 export interface Middleware<
-    DispatchExt = {},
-    S = any,
-    D extends Dispatch = Dispatch
-    > {
-    (api: MiddlewareAPI<D, S>): (
-        next: Dispatch<AnyAction>
-    ) => (action: any) => any
+  DispatchExt = {},
+  S = any,
+  D extends Dispatch = Dispatch
+> {
+  (api: MiddlewareAPI<D, S>): (
+    next: Dispatch<AnyAction>
+  ) => (action: any) => any;
 }
 
 export interface MiddlewareAPI<D extends Dispatch = Dispatch, S = any> {
-    dispatch: D
-    getState(): S
+  dispatch: D;
+  getState(): S;
 }
 
 export interface SignalDispatch<S, A extends Action> {
-    <T extends A>(action: T): T;
-    <R>(asyncAction: SignalAction<R, S, A>): R;
+  <T extends A>(action: T): T;
+  <R>(asyncAction: SignalAction<R, S, A>): R;
 }
 
 export type SignalAction<R, S, A extends Action> = (
-    dispatch: SignalDispatch<S, A>,
-    getState: () => S,
-    invoke: signalR.HubConnection['invoke']
+  dispatch: SignalDispatch<S, A>,
+  getState: () => S,
+  invoke: signalR.HubConnection["invoke"]
 ) => R;
 
-export type SignalMiddleware<S = {}, A extends Action = AnyAction> = Middleware<SignalDispatch<S, A>, S, SignalDispatch<S, A>>;
+export type SignalMiddleware<S = {}, A extends Action = AnyAction> = Middleware<
+  SignalDispatch<S, A>,
+  S,
+  SignalDispatch<S, A>
+>;
 
 export interface MiddlewareConfig {
-    callbacks: ReturnType<typeof withCallbacks>,
-    connection: signalR.HubConnection,
-    onStart?(): void;
+  callbacks: ReturnType<typeof withCallbacks>;
+  connection: signalR.HubConnection;
+  onStart?(): void;
 }
